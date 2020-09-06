824d1888e8b473eb48aad0b76e5e0f11ebd93efc
Héctor Tello <hectortllo@gmail.com>
2020-09-05 19:36:58 -0600

    1. Servidor local listo. 2. Compilar estilos listo. 3. Minificar archivos css listo. 4. Sincronizar cambios en el navegador listo. 5. Falta minificar archivos js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,67 @@
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const uglifycss = require('gulp-uglifycss');
+const browserSync = require('browser-sync');
 
-gulp.task('sass', function() {
-  return gulp.src('./src/sass/*.sass')
+const files = {
+  sassPath: 'src/sass/*.sass',
+  cssPath: 'dist/css/*.css',
+  cssMinifiedPath: 'dist/css/minified'
+}
+
+function compileSASS() {
+  console.log('compileSASS');
+  return src(files.sassPath)
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./dist/css'));
-});
+  .pipe(dest('dist/css'));
+}
 
-gulp.task('css', function () {
-  gulp.src('./dist/css/*.css')
+function minifyCSS() {
+  console.log('minifyCSS')
+  return src(files.cssPath)
     .pipe(uglifycss({
-      "maxLineLen": 80,
       "uglyComments": true
     }))
-    .pipe(gulp.dest('./dist/css/minified'));
-});
+  .pipe(dest('dist/minified'));
+}
+
+function watchTask() {
+  watch(
+    [files.sassPath, files.cssMinifiedPath],
+    series(compileSASS, minifyCSS, reloadTask)
+  )
+  console.log('watchTask')
+}
+
+function serveTask(d) {
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+  console.log('serveTask');
+  d();
+}
+
+function reloadTask(d) {
+  browserSync.reload();
+  d();
+}
+
+exports.default = series(compileSASS, minifyCSS, serveTask, watchTask)
+
+
+// exports.sass = compileSASS;
+// exports.css = minifyCSS;
+
+
+// exports.default = series(scssTask, serveTask, watchTask);
+
+// gulp.task('run', ['sass', 'css']);
+
+// gulp.task('watch', function() {
+//   gulp.watch('./src/sass/*.sass', ['sass']);
+//   gulp.watch('./dist/css/*.css', ['css']);
+// });
+
+// gulp.task('default', ['run', 'watch']);
